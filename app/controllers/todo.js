baeb8ac4c2f762a303a18b9d6893515fe197cf51
Castle Crawford <nathanscottcrawford@gmail.com>
2016-06-01 11:16:26 -0500

    basic intro to angular demo

--- app/controllers/todo.js
+++ app/controllers/todo.js
@@ -1,28 +1,49 @@
-app.controller("TodoCtrl", function($scope) {
+'use strict';
+
+app.controller("TodoCtrl", ($scope, $http) => {
 
   $scope.title = 'Angular Todo - PFM!';
-  $scope.filter2 ='';
+  $scope.filter2 = '';
   $scope.potato = '';
   $scope.variableName = '';
 
-  $scope.newTodo ='';
+  $scope.newTodo = '';
+  $scope.taskType = 'home';
 
-  $scope.tasks = [
-    { name: "Make the bed", type: "home"},
-    { name: "Eat breakfast", type: "home"},
-    { name: "Check the network", type: "work"},
-    { name: "Start up Slack", type: "work"},
-    { name: "Feed the cats", type: "home"}
-  ];
+  $scope.selectedTodo= 'nothing';
 
-  $scope.addTodo = function() {
-    $scope.tasks.push({ name: $scope.newTodo, type: "home"});
-    $scope.newTodo ='';
+  // $scope.tasks = [
+  //   { name: "Make the bed", type: "home"},
+  //   { name: "Eat breakfast", type: "home"},
+  //   { name: "Check the network", type: "work"},
+  //   { name: "Start up Slack", type: "work"},
+  //   { name: "Feed the cats", type: "home"}
+  // ];
+
+$http.get('data/tasks.json')
+.then((response) => {
+  console.log("response",response);
+  $scope.tasks = response.data.tasks;
+})
+
+  // $http.post('/data/tasks.json', {data: "what I'm sending"})
+  //   .then((response) => {
+  //     console.log("response", response);
+  //     $scope.tasks = response.data.tasks;
+  // })
+
+
+  $scope.addTodo = () => {
+    $scope.tasks.push({ name: $scope.newTodo, type: $scope.taskType});
+    $scope.newTodo = '';
   }
 
   $scope.removeTodo = (task) => {
     const taskIndex = $scope.tasks.indexOf(task);
-    $scope.tasks.splice(taskIndex, 1);
+    if (taskIndex >= 0) {
+      $scope.tasks.splice(taskIndex, 1); // eslint-disable-line no-magic-numbers
+    }
   }
 
 })
+
